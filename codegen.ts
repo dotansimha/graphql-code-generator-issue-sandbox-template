6aedb9ce54a075f626bbdde03e37892d9cb3675f
Charly POLY <cpoly55@gmail.com>
2022-10-24 11:59:43 +0200

    refactor: update to latest codegen/cli and best practices

---
+++ codegen.ts
@@ -0,0 +1,11 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "schema.graphql",
+  documents: "document.graphql",
+  generates: {
+    "types.ts": { plugins: ["typescript", "typescript-operations"] },
+  },
+};
+
+export default config;
--- types.ts
+++ types.ts
@@ -1,1 +1,35 @@
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
 
+export type Query = {
+  __typename?: 'Query';
+  user: User;
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['ID'];
+};
+
+export type User = {
+  __typename?: 'User';
+  email: Scalars['String'];
+  id: Scalars['ID'];
+  username: Scalars['String'];
+};
+
+export type UserQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type UserQuery = { __typename?: 'Query', user: { __typename?: 'User', id: string, username: string, email: string } };
